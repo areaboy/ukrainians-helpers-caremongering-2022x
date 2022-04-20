78a9d037b491116e90144955288a502c95e4db22
Esedo Fredrick <50068759+areaboy@users.noreply.github.com>
2022-04-20 21:54:34 +0100

    Add files via upload

---
+++ ukrainians_helpers_caremongering/footer_title.php
@@ -0,0 +1,8 @@
+﻿<?php
+
+$footer_tit1=  "An Interactive System that brings and connect People, Communities and the World together to help those Ukrainians in needs..";
+
+$footer_tit2 ="";
+
+$footer_tit3 ="";
+?>
---
+++ ukrainians_helpers_caremongering/header_title.php
@@ -0,0 +1,21 @@
+﻿<?php
+$header_tit=  "Ukrainians Helpers CareMongerings";
+$invite_member ="Coming Soon";
+$poster ="Posts";
+$poster2 ="Help";
+$poweredby ="Graph For All Million Dollar Challenge";
+$heading ="<h3>An Interactive and Rewarding System that brings and connect People, various Communities and the World together to help those Ukrainians in needs..</h3>
+
+<h4>
+<br>Inspired by mutual help and 
+caremongering movements catalyzed by COVID-19 Pandemic.
+
+
+It is a platform that brings together those Ukrainians who need help  with those who are willing to help them for everyday thing rangings from
+ <b>
+Foods, Gloceries Delivery, Shelter, Advice, Eldery Care, Child Care, Transportaions, Grocery Shoppings, Opportunities/Careers,
+ Professional Services,Handy Work, Jobs, Health-Care, Insurance, Business, Environments, Others</b><br>
+<br>
+
+Our applications awards more Points to appreciate those who gives help the most.";
+?>
